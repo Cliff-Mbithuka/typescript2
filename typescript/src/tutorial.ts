155e86d6ae7ee0fc7d3a38abf2ad7b81e93eb436
Clifford Musyoka <your_email@example.com>
2024-06-18 23:15:18 +0300

    type annotations

---
+++ typescript/src/tutorial.ts
@@ -0,0 +1,18 @@
+let awesomeName: string = "shakeAndBake";
+
+awesomeName = 'something'
+
+awesomeName = awesomeName.toLocaleUpperCase()
+
+console.log(awesomeName);
+// awesomeName = 20
+
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount = 'pants';
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = "shakeAndBake"
+
