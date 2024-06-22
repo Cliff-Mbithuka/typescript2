e7854986cf1352b58257a12ee94d6b3c06e932d3
Clifford Musyoka <your_email@example.com>
2024-06-22 23:16:28 +0300

    type Alias

--- typescript/src/tut4.ts
+++ typescript/src/tut4.ts
@@ -57,6 +57,7 @@ printStaffDetails(bob)
 
 
 type Book = {id: number; name: string;price: number};
+type DiscountedBook =  Book & {discount: number}
 
 const book1: Book = {
     id: 1,
@@ -68,10 +69,15 @@ const book2: Book = {
     name: 'The secret life of unicorns',
     price: 18,
 };
-const discountedbook: Book & {discount: number} = {
+const discountedbook: DiscountedBook = {
     id: 3,
     name: 'Gnomes vs. Goblins: The Ultimate Guide',
     price: 25,
     discount: 0.15,
 };
 
+const propName = 'age';
+type Animal = {
+    [propName]: number;
+};
+let tiger: Animal = { [propName]: 5}
