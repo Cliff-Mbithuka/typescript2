509ae92eae2060cf595a7956ba937449d4c901dd
Clifford Musyoka <your_email@example.com>
2024-06-22 15:14:52 +0300

    use of rest Parameters

--- typescript/src/tut3.ts
+++ typescript/src/tut3.ts
@@ -34,3 +34,32 @@ if(isNameInList(nameToCheck)){
     console.log((`${nameToCheck} is not in the list`));
     
 }
+
+//OPtional and default Parameters
+function calculatePrice(price: number, discount?: number):number{
+    return price - (discount || 0);
+}
+
+let priceAfterDiscount = calculatePrice(100, 20);
+
+function calculateScore(initialScore: number, penaltyPoints: number = 0):number{
+    return initialScore - penaltyPoints;
+}
+
+let scoreAfterPenalty = calculateScore(100,20)
+let scoreWithoutPenalty = calculateScore(300)
+
+//Rest Parameters
+function sum(message:string, ...numbers:number[]):string{
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled);
+
+
+let total = numbers.reduce((previous, current) => {
+    return previous + current;
+}, 0);
+return `${message}${total}`;
+}
+
+let result = sum('The total is : ',1,2,3,4,5)
+console.log(result);
